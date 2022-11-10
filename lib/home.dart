55f7d99bd70fd682f1061b86544a5d58fbed65e0
NandavardhanRadhakrishnan <nr3648@srmist.edu.in>
2022-11-10 23:53:42 +0530

    Final Changes

--- lib/home.dart
+++ lib/home.dart
@@ -14,37 +14,42 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Color.fromRGBO(0, 128, 105, 1),
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text('WhatsApp'),
-              SizedBox(width: 130,),
-              Icon(Icons.search),
-              Icon(Icons.more_vert),
-            ],
-          ),
-          bottom: TabBar(
-            indicatorColor: Colors.white,
-            tabs: [
-              Tab(text: 'CHAT'),
-              Tab(text: 'CALLS'),
-              Tab(text: 'STATUS')
-            ],
-          ),
+        length: 3,
+        child: Scaffold(
+            appBar: AppBar(
+              backgroundColor: Color.fromRGBO(0, 128, 105, 1),
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('WhatsApp'),
+                  SizedBox(width: 130,),
+                  Icon(Icons.search),
+                  Icon(Icons.more_vert),
+                ],
+              ),
+              bottom: TabBar(
+                indicatorColor: Colors.white,
+                tabs: [
+                  Tab(text: 'CHAT'),
+                  Tab(text: 'CALLS'),
+                  Tab(text: 'STATUS')
+                ],
+              ),
 
-        ),
-        body: TabBarView(
-          children: [
-            Chat(),
-            Calls(),
-            Status()
-          ],
-        ),
-      ),
+            ),
+            body: TabBarView(
+              children: [
+                Chat(),
+                Calls(),
+                Status()
+              ],
+            ),
+            floatingActionButton: FloatingActionButton(
+              child: Icon(Icons.chat_bubble, color: Colors.white,),
+              onPressed: () {},
+              backgroundColor: Colors.green,)
+        )
+
     );
   }
 }
